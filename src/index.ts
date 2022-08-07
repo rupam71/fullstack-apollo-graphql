247c0d60667b83556d1d11b4a7ad555714014c1e
Rupam Sahriar <rupamsahriar@gmail.com>
2022-08-07 17:41:23 +0600

    inilialize apollo server with demo typedefs and resolver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('From Typescript file')
+import { ApolloServer } from 'apollo-server-express';
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
+
+async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+startApolloServer()
---
+++ src/typeDefs/index.ts
@@ -0,0 +1,13 @@
+import { ApolloServer, gql } from "apollo-server-express";
+
+const typeDefs = gql`
+    type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+export default typeDefs;
