5bfb98529f0d48bdce35b284c46015872059539c
Rupam Sahriar <rupamsahriar@gmail.com>
2022-08-08 16:37:32 +0600

    GraphQL Resolvers

--- src/typeDefs/index.ts
+++ src/typeDefs/index.ts
@@ -1,12 +1,34 @@
-import { ApolloServer, gql } from "apollo-server-express";
+import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
-    type Book {
-    title: String
-    author: String
+  type Book {
+    id: ID!
+    title: String!
+    author: Author!
   }
-  type Query {
+  type Author {
+    id: ID!
+    name: String!
     books: [Book]
+  }
+  input addBookInput {
+    title: String!
+    author: ID!
+  }
+  input addAuthorInput {
+    name: String!
+  }
+
+  type Query {
+    getAllbooks: [Book] # /api/books
+    getBookById(id: ID!): Book # /api/books/:id
+    getAuthors: [Author] # /api/authors
+    getAuthorById(id: ID!): Author # /api/authors/:id
+  }
+  
+  type Mutation {
+    addBook(addBookInput: addBookInput!): Book
+    addAuthor(addAuthorInput: addAuthorInput!): Author
   }
 `;
 
