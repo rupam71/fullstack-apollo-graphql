456054017f73e033abc660fab5e598ab2d295daa
Rupam Sahriar <rupamsahriar@gmail.com>
2022-08-08 16:36:41 +0600

    GraphQL typedefs file

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,18 +1,37 @@
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
+import Author from "../models/AuthorModel";
+import Book from "../models/BookModel";
 
-  const resolvers = {
-    Query: {
-      books: () => books,
-    },
-  };
+const resolvers = {
+  Query: {
+    getAllbooks: async () => await Book.find(),
+    getBookById: async (parent: any, args: any) => await Book.findById(args.id),
+    getAuthors: async () => await Author.find(),
+    getAuthorById: async (parent: any, args: any) =>
+      await Author.findById(args.id),
+  },
+  Book: {
+    author: async (parent: any) => await Author.findById(parent.author),
+  },
+  Author: {
+    books: async (parent: any) => await Book.find({ author: parent.id }),
+  },
+  Mutation: {
+    addBook: async (parent: any, args: any, context: any, info: any) => {
+      const book = new Book({
+        title: args.addBookInput.title,
+        author: args.addBookInput.author,
+      });
 
-  export default resolvers;
+      return await book.save();
+    },
+    addAuthor: async (parent: any, args: any, context: any, info: any) => {
+      const author = new Author({
+        name: args.addAuthorInput.name,
+      });
+
+      return await author.save();
+    },
+  },
+};
+
+export default resolvers;
