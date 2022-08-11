5534be6e138ad2e80fcee2f0624bb18ac40c407f
Rupam Sahriar <rupamsahriar@gmail.com>
2022-08-11 15:12:31 +0600

    GraphQL schema file

--- src/typeDefs/index.ts
+++ src/typeDefs/index.ts
@@ -11,6 +11,7 @@ const typeDefs = gql`
     name: String!
     books: [Book]
   }
+  
   input addBookInput {
     title: String!
     author: ID!
@@ -19,16 +20,52 @@ const typeDefs = gql`
     name: String!
   }
 
+  input updateBookInput {
+    id: ID!
+    title: String
+    author: ID
+  }
+  input updateAuthorInput {
+    id: ID!
+    name: String
+  }
+
+  type BooksResult {
+    book: [Book]
+    count: Int
+  }
+  type AuthorsResult {
+    author: [Author]
+    count: Int
+  }
+
+  type BooksResponse {
+    statusCode: Int!
+    message: String!
+    result: BooksResult
+  }
+  type AuthorsResponse {
+    statusCode: Int!
+    message: String!
+    result: AuthorsResult
+  }
+
   type Query {
+    getBooks(id: ID): BooksResponse
     getAllbooks: [Book] # /api/books
     getBookById(id: ID!): Book # /api/books/:id
-    getAuthors: [Author] # /api/authors
+    getAuthors(id: ID): AuthorsResponse
+    getAllAuthors: [Author] # /api/authors
     getAuthorById(id: ID!): Author # /api/authors/:id
   }
   
   type Mutation {
     addBook(addBookInput: addBookInput!): Book
     addAuthor(addAuthorInput: addAuthorInput!): Author
+    updateBook(updateBookInput: updateBookInput!): Book
+    updateAuthor(updateAuthorInput: updateAuthorInput!): Author
+    deleteBook(bookId: ID!): Book
+    deleteAuthor(authorId: ID!): Author
   }
 `;
 
