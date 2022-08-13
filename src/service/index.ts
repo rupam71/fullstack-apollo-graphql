debcec1927895300ecc0c4df4349e27c4d668382
Rupam Sahriar <rupamsahriar@gmail.com>
2022-08-13 17:42:54 +0600

    User import add on root file

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,4 +1,5 @@
 import { BookTypes, BookQuery, BookMutation, BookResolvers } from './book';
+import { UserTypes, UserQuery, UserMutation, UserResolvers } from './user';
 import { gql } from 'apollo-server-express';
 import { AuthorTypes, AuthorQuery, AuthorMutation, AuthorResolvers } from './author';
 
@@ -7,16 +8,19 @@ export const typeDefs = gql`
     type Mutation
     ${BookTypes}
     ${AuthorTypes}
+    ${UserTypes}
 `
 
 export const resolvers = {
     Query: {
         ...BookQuery,
         ...AuthorQuery,
+        ...UserQuery,
     },
     Mutation: {
         ...BookMutation,
         ...AuthorMutation,
+        ...UserMutation,
     },
     Book: {
         ...BookResolvers,
