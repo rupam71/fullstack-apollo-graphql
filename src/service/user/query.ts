2282cdae06f3ba6cdccecea7e118b02c43080459
Rupam Sahriar <rupamsahriar@gmail.com>
2022-08-14 14:06:42 +0600

    create access token and use access token

--- src/service/user/query.ts
+++ src/service/user/query.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 import { sendUserResponse } from "./helper";
 import User from "./model";
+import jwt from "jsonwebtoken"
 
 export const UserQuery = {
-  getAllUser: async () => await User.find(),
-  getUserById: async (parent: any, args: any) => {
+  getAllUser: async (parent: any, args: any, context:any) => {
+    if(!context.user)  return sendUserResponse(403,"Permission Denied",{})
+    else return await User.find()
+  },
+  getUserById: async (parent: any, args: any, context:any) => {
+    if(!context.user)  return sendUserResponse(403,"Permission Denied",{})
     if(!mongoose.Types.ObjectId.isValid(args.id)) return sendUserResponse(403,"Invalid ID",{})
 
     const user = await User.findById(args.id);
@@ -17,6 +22,9 @@ export const UserQuery = {
 
     if(user.length===0) return sendUserResponse(401,"Email Not Match")
     else if(user[0].password!==args.password) return sendUserResponse(401,"Password Not Match")
-    else return sendUserResponse(200,"Success",user[0])
+    else {
+      const token = jwt.sign({ _id: user[0]._id.toString() },process.env.JWT as string)
+      return sendUserResponse(200,"Success",user[0],token)
+    }
   },
 };
